40d6c6afdb2e93c865e6fabfb0fafe6d4fc7c89d
Matthew Wendzina <mattwendzina@gmail.com>
2020-12-31 11:15:23 +0000

    Add toggleCheck action

--- src/store/actions/items.js
+++ src/store/actions/items.js
@@ -3,6 +3,7 @@ import helpers from '../../helperFunctions';
 import { loadAllLists } from './lists';
 
 export const ON_SUBMIT = 'ON_SUBMIT';
+export const ON_CHECK = 'ON_CHECK';
 
 export const onSubmit = (event, item, selectedList) => {
     event.preventDefault();
@@ -12,6 +13,19 @@ export const onSubmit = (event, item, selectedList) => {
                 id: helpers.randomId(),
                 name: item,
                 checked: false,
+            })
+            .then((res) => {
+                dispatch(loadAllLists());
+            })
+            .catch((e) => console.log(e));
+    };
+};
+
+export const toggleCheck = (itemId, checked, selectedList) => {
+    return (dispatch) => {
+        axios
+            .patch(`/lists/${selectedList[0]}/items/${itemId}/.json`, {
+                checked: !checked,
             })
             .then((res) => {
                 dispatch(loadAllLists());
