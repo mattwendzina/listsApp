857ed8ce7894b34e915b1988d20cb1f7a482cc28
Matthew Wendzina <mattwendzina@gmail.com>
2021-01-09 15:30:12 +0000

    Add Connect() and refactor so that it now has two separate components

--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,91 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import classes from './toolbar.module.css';
-
-import Input from '../../UI/Input/InputEl';
-
-import { FiEdit2, FiSave } from 'react-icons/fi';
-import axios from '../../../axios-shoppingList';
+import Title from '../../Title/Title';
+import Menu from '../../Menu/Menu';
+import { setNewTitle } from '../../../store/actions/lists';
 
 const Toolbar = (props) => {
-    const [editMode, toggleEdit] = useState(false);
-    const [title, changeTitle] = useState();
-
-    useEffect(
-        function loadTitle() {
-            changeTitle(props.listName);
-        },
-        [props]
-    );
-
-    const updateTitle = (e) => {
-        changeTitle(e.target.value);
-    };
-
-    const editTitle = () => {
-        toggleEdit(!editMode);
-    };
-
-    const submitNewTitle = () => {
-        axios
-            .patch(`/lists/${props.listId}/.json`, {
-                name: title,
-            })
-            .then(() => props.getLists(props.listId))
-            .catch((e) => console.log(e));
-        toggleEdit(!editMode);
-    };
-
-    const onBlur = (input) => {
-        if (input.key && input.key !== 'Enter') {
-            return;
-        }
-        submitNewTitle();
-        toggleEdit(!editMode);
-    };
-
     return (
-        <header className={classes.appHeader}>
-            <div className={classes.headerContainer}>
-                {!editMode ? (
-                    <h2>{title}</h2>
-                ) : (
-                    <Input
-                        elementType="input"
-                        autoFocus="autofocus"
-                        blur={onBlur}
-                        keyUp={onBlur}
-                        value={title}
-                        changed={updateTitle}
-                        className="headerInput"
-                        color="blue"
-                    />
-                )}
-                {!editMode ? (
-                    <FiEdit2 className={classes.editIcon} onClick={editTitle} />
-                ) : (
-                    <FiSave
-                        className={classes.saveIcon}
-                        onClick={submitNewTitle}
-                    />
-                )}
-            </div>
-            <nav>
-                <ul className={classes.navItems}>
-                    <li className={classes.seeAllLists}>
-                        <Link to="/allLists">See all lists</Link>
-                    </li>
-                    <li
-                        className={classes.newList}
-                        onClick={props.createNewList}
-                    >
-                        <a href="/newList">New List</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <div className={classes.toolbar}>
+            {props.location.pathname === '/list' && (
+                <Title
+                    createNewList={props.createNewList}
+                    seeAllLists={props.seeAllLists}
+                    listName={props.selectedList && props.selectedList.name}
+                    listId={props.selectedList && props.selectedList.listId}
+                    setNewTitle={props.setNewTitle}
+                />
+            )}
+            <Menu />
+        </div>
     );
 };
 
-export default Toolbar;
+const mapStateToProps = (state) => {
+    return {
+        selectedList: state.lists.selectedList,
+    };
+};
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setNewTitle: (listId, title) => dispatch(setNewTitle(listId, title)),
+    };
+};
+
+export default withRouter(
+    connect(mapStateToProps, mapDispatchToProps)(Toolbar)
+);
