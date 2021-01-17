7e5a7d9bcced00b39aba7528de85a4ee309b8ed7
Matthew Wendzina <mattwendzina@gmail.com>
2021-01-17 17:12:25 +0000

    Use new Button component

--- src/Components/ConfirmDelete/ConfirmDelete.js
+++ src/Components/ConfirmDelete/ConfirmDelete.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './ConfirmDelete.module.css';
+import Button from '../UI/Button/Button';
 
 const ConfirmDelete = (props) => {
     return (
         <div className={classes.confirmDelete}>
             <h3>Are you sure you wish to delete</h3>
             <div className={classes.buttonContainer}>
-                <button onClick={props.confirmDelete}>Yes</button>
-                <button onClick={props.cancelDelete}>No</button>
+                <Button onClick={props.confirmDelete}>Yes</Button>
+                <Button onClick={props.cancelDelete}>No</Button>
             </div>
         </div>
     );
