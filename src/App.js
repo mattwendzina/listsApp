d883e87c5e4e2859755e2721cb9793e6b594b072
Matthew Wendzina <mattwendzina@gmail.com>
2021-01-22 13:21:46 +0000

    Remove local state, newList methods and small refactors

--- src/App.js
+++ src/App.js
@@ -12,13 +12,6 @@ import Layout from './Components/Layout/Layout';
 
 import { loadAllLists } from './store/actions/lists';
 class App extends Component {
-    state = {
-        listId: null,
-        allLists: [],
-        itemToEdit: null,
-        editMode: { edit: false, id: null },
-    };
-
     componentDidMount() {
         if (!localStorage.getItem('listId')) {
             this.props.history.push('/');
@@ -26,65 +19,11 @@ class App extends Component {
         this.props.getAllLists();
     }
 
-    createNewList = () => {
-        this.setState({
-            newListEditMode: !this.state.newListEditMode,
-        });
-    };
-
-    submitNewList = () => {
-        axios
-            .post('/lists.json', {
-                name: this.state.inputElements.newList.value,
-                items: this.state.newListItems,
-                itemId: helpers.randomId(),
-            })
-            .then((res) => {
-                this.getLists(this.state.listId);
-                this.setState(
-                    {
-                        inputElements: {
-                            newItem: {
-                                elementType: 'input',
-                                elementConfig: {
-                                    type: 'text',
-                                    placeholder: 'New Item',
-                                },
-                                value: {
-                                    itemId: helpers.randomId(),
-                                    item: null,
-                                    checked: false,
-                                },
-                            },
-                            newList: {
-                                elementType: 'input',
-                                elementConfig: {
-                                    type: 'text',
-                                    placeholder: 'New Item',
-                                },
-                                value: null,
-                            },
-                        },
-                    },
-                    () => {
-                        console.log('this.state post submit', this.state);
-                    }
-                );
-            })
-            .catch((e) => console.log(e));
-    };
-
     render() {
         return (
             <div className="App">
                 {this.props.error ? <h1>{this.props.error}</h1> : null}
-                <Layout
-                    createNewList={this.createNewList}
-                    listName={
-                        this.props.selectedList && this.props.selectedList.name
-                    }
-                    listId={this.state.listId}
-                >
+                <Layout>
                     <Route path="/" exact component={() => <ListsOverview />} />
                     <Route path="/list" exact component={() => <ListView />} />
                     <Route
@@ -98,29 +37,15 @@ class App extends Component {
     }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        selectedList: state.lists.selectedList,
-        error: state.lists.errorMessage,
-    };
-};
+const mapStateToProps = (state) => ({
+    selectedList: state.lists.selectedList,
+    error: state.lists.errorMessage,
+});
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        getAllLists: (items) => {
-            dispatch(loadAllLists(items));
-        },
-    };
-};
+const mapDispatchToProps = (dispatch) => ({
+    getAllLists: (items) => {
+        dispatch(loadAllLists(items));
+    },
+});
 
-const mergeProps = (state, dispatch, ownProps) => {
-    return {
-        ...state,
-        ...dispatch,
-        ...ownProps,
-    };
-};
-
-export default withRouter(
-    connect(mapStateToProps, mapDispatchToProps, mergeProps)(App)
-);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
