fbf81520ca4267bbb894ac21111e58d8a1721d81
Matthew Wendzina <mattwendzina@gmail.com>
2021-01-22 13:23:33 +0000

    Remove logic processing out of component

--- src/Components/ListItems/ListItems.js
+++ src/Components/ListItems/ListItems.js
@@ -9,64 +9,31 @@ import Modal from '../UI/Modal/Modal';
 import Backdrop from '../UI/Backdrop/Backdrop';
 import ConfirmDelete from '../ConfirmDelete/ConfirmDelete';
 
-const ListItems = (props) => {
-    const {
-        selectedList,
-        items,
-        newListEditMode,
-        addItemToNewList,
-        deleteItem,
-        deleteWarning,
-        deleteWarningMessage,
-        toggleEdit,
-        toggleCheck,
-    } = props;
-    const [localCheckedStatus, updateLocalCheckedStatus] = useState();
-    const [itemToDelete, setItemToDelete] = useState();
-
-    // I want to be able to update the checkedItem status instantly, rather than relying on dispatching a reload of the lists from the server as this creates a short delay. Hence using useEffect()
-    useEffect(() => {
-        updateLocalCheckedStatus(() => {
-            const localCheckedStatus = [];
-            if (selectedList) {
-                items.forEach(({ checked, id }) => {
-                    return localCheckedStatus.push({ id, checked });
-                });
-            }
-            return localCheckedStatus;
-        });
-    }, [selectedList, items]);
-
+const ListItems = ({
+    selectedList,
+    items,
+    itemToDelete,
+    deleteItem,
+    deleteWarning,
+    deleteWarningMessage,
+    toggleEdit,
+    toggleCheck,
+}) => {
     const formattedItems = [];
 
-    if (selectedList) {
-        items.forEach(({ name, id, checked, itemId }, idx) => {
+    selectedList &&
+        items.forEach(({ name, id, checked, itemId }) => {
             return formattedItems.push(
                 <div key={id} className={classes.listItem}>
-                    {newListEditMode && (
-                        <input
-                            type="checkbox"
-                            onClick={() => addItemToNewList(name, id)}
-                        />
-                    )}
-                    {!newListEditMode && (
-                        <AiFillDelete
-                            className={classes.delete}
-                            onClick={() => {
-                                deleteItem(deleteWarning);
-                                setItemToDelete(() => ({
-                                    id,
-                                    listId: selectedList.listId,
-                                }));
-                            }}
-                        />
-                    )}
+                    <AiFillDelete
+                        className={classes.delete}
+                        onClick={() =>
+                            deleteWarningMessage({ deleteWarning: true, id })
+                        }
+                    />
                     <li
                         className={
-                            // When adding a new Item, there is no checkedStatus, hence the first check, then on initial load there is no checked property, hence the check for checkedStatus[idx]
-                            localCheckedStatus &&
-                            localCheckedStatus[idx] &&
-                            localCheckedStatus[idx].checked
+                            checked
                                 ? classes.checkedItem
                                 : classes.uncheckedItem
                         }
@@ -74,59 +41,31 @@ const ListItems = (props) => {
                     >
                         {name}
                     </li>
-                    {localCheckedStatus &&
-                    localCheckedStatus[idx] &&
-                    localCheckedStatus[idx].checked ? (
+                    {checked ? (
                         <RiCheckboxCircleLine
                             className={classes.checked}
-                            onClick={() => {
-                                updateLocalCheckedStatus((items) => {
-                                    const updatedStatus = items.map((item) => {
-                                        if (item.id === id) {
-                                            return {
-                                                ...item,
-                                                checked: !item.checked,
-                                            };
-                                        }
-                                        return item;
-                                    });
-                                    return updatedStatus;
-                                });
-                                toggleCheck(itemId, checked, selectedList);
-                            }}
+                            onClick={() =>
+                                toggleCheck(itemId, checked, selectedList)
+                            }
                         />
                     ) : (
                         <RiCheckboxBlankCircleLine
                             className={classes.unchecked}
-                            onClick={() => {
-                                updateLocalCheckedStatus((items) => {
-                                    const updatedStatus = items.map((item) => {
-                                        if (item.id === id) {
-                                            return {
-                                                ...item,
-                                                checked: !item.checked,
-                                            };
-                                        }
-                                        return item;
-                                    });
-                                    return updatedStatus;
-                                });
-                                toggleCheck(itemId, checked, selectedList);
-                            }}
+                            onClick={() =>
+                                toggleCheck(itemId, checked, selectedList)
+                            }
                         />
                     )}
                 </div>
             );
         });
-    }
+
     return (
         <div>
             <ul className={classes.listItemsContainer}>{formattedItems}</ul>
             <Modal showModal={deleteWarning}>
                 <ConfirmDelete
-                    confirmDelete={() =>
-                        deleteItem(deleteWarning, selectedList, itemToDelete)
-                    }
+                    confirmDelete={() => deleteItem(selectedList, itemToDelete)}
                     cancelDelete={() => deleteWarningMessage(false)}
                 />
             </Modal>
